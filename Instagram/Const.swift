e56130f7231da56fefd869f92d5c55a4e91e8aab
kgoat <kgoat2020@gmail.com>
2020-10-21 12:23:30 +0900

    インスタクローンアプリ作成

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Kazuyo Kohama on 2020/10/16.
+//  Copyright © 2020 k.goat. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
